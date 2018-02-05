ad92a445d949e47974243e33f0c38c84bbcc0a0c
TanDatDo <dat.do@aalto.fi>
2018-02-05 03:20:48 +0200

    Initial commit

---
+++ designs.js
@@ -0,0 +1,24 @@
+// Select color input
+var colorPicker=$('#colorPicker').val();
+// Select size input
+var inputHeight=$('#inputHeight').val();
+var inputWeight=$('#inputWeight').val();
+
+$('#submit').click(function(e){
+  $('#pixelCanvas tr').remove();
+  var inputHeight=$('#inputHeight').val();
+  var inputWeight=$('#inputWeight').val();
+  for (i = 0; i < inputHeight; i++) {
+  $('#pixelCanvas').append('<tr></tr>');
+  }
+  for (i = 0; i < inputWeight; i++) {
+  $('#pixelCanvas tr:first ').append("<td class='TableHeading'></td>");
+  $('#pixelCanvas tr:not(:first)').each(function(){
+  $(this).append("<td></td>");});
+     }
+  e.preventDefault();
+});
+$('#pixelCanvas').on('mouseenter', 'td', function(){
+  var colorPicker=$('#colorPicker').val();
+  $(this).css('background', colorPicker);
+});
